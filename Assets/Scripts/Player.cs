c682abbfecf41230f34cf8eb6dd266b8a82ac985
ShoomUW <falmars.g@gmail.com>
2022-01-22 05:27:07 +0900

    プレイヤー移動のスクリプトを修正

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,19 +16,22 @@ public class Player : MonoBehaviour
     void Start()
     {
         this.transform.position = spawn;
+        this.transform.rotation = Quaternion.AngleAxis(0f, spawn);
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool flag = false;
+        Vector3 move = Vector3.zero;
         /*
          * 前方に力を加えることで移動させる
          * Vector3型変数moveに前方向のベクトルを代入
          */
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 move = this.transform.forward * maxspeed;
-            this.GetComponent<Rigidbody>().velocity = move;
+            flag = true;
+            move += this.transform.forward * maxspeed;
         }
         /*
          * 左に力を加えることで移動させる
@@ -36,26 +39,36 @@ public class Player : MonoBehaviour
          */
         if (Input.GetKey(KeyCode.A))
         {
-            Vector3 move = -this.transform.right * maxspeed;
-            this.GetComponent<Rigidbody>().velocity = move;
+            flag = true;
+            move += -this.transform.right * maxspeed;
         }
         /*
          * 右に力を加えることで移動させる
          * Vector3型変数moveに右方向のベクトルを代入
          */
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D))
         {
-            Vector3 move = this.transform.right * maxspeed;
-            this.GetComponent<Rigidbody>().velocity = move;
+            flag = true;
+            move += this.transform.right * maxspeed;
         }
         /*
          * 後方に力を加えることで移動させる
          * Vector3型変数moveに後方向のベクトルを代入
          */
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.S))
         {
-            Vector3 move = -this.transform.forward * maxspeed;
-            this.GetComponent<Rigidbody>().velocity = move;
+            flag = true;
+            move += -this.transform.forward * maxspeed;
         }
+        /*
+         * フラグがfalseの場合、速度を0にする
+         */
+        if (!flag)
+        {
+            move = Vector3.zero;
+        }
+
+        this.GetComponent<Rigidbody>().velocity = move;
+
     }
 }
